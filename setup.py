4bbb315cb0e847c046d58a7d22378b27d745aaa1
Oleg Morozenkov <m@oleg.rocks>
2019-01-01 03:18:12 +0300

    [*] fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gentle-python-pptx',
-    version='0.4.0',
+    version='0.4.1',
 
     description='Processes pptx presentations, optimized for repetitive processing large ones',
     long_description=open('README.md').read(),
@@ -11,6 +11,7 @@ setup(
     author_email='o.morozenkov@ktsstudio.ru',
     license='MIT',
     packages=find_packages(),
+    include_package_data=True,
 
-    install_requires=['lxml>=4.2.5,<4.3', 'Pillow>=5.2.0,<5.3']
+    install_requires=['lxml>=4.2.5,<4.3', 'Pillow>=5.2.0,<5.3', 'XlsxWriter>=1.1.2,<1.2']
 )
